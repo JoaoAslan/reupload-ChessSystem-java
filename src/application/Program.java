8f745a5c523477f468b8ac21d9fc1b5856dffa3a
JoaoAslan <contato.joaoaslan@gmail.com>
2022-10-21 21:39:53 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardlayer.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position p = new Position(5,3);
+        System.out.println(p);
+        
+    }
+}
---
+++ src/boardlayer/Position.java
@@ -0,0 +1,34 @@
+package boardlayer;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(row);
+        sb.append(", ");
+        sb.append(column);
+        return sb.toString();
+    }
+}
