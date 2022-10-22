cd4915cdd070a270b121cf83958275c43c226346
JoaoAslan <contato.joaoaslan@gmail.com>
2022-10-22 12:01:56 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,16 @@
 package application;
 
 import chesslayer.ChessPiece;
+import chesslayer.enums.Color;
 
 public class UI {
+
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
@@ -20,7 +28,12 @@ public class UI {
             System.out.print("-");
         }
         else {
-            System.out.print(piece);
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         System.out.print(" ");
     }
